f95154b8655c3a81ba8ffc866aea5e3af97e7df8
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-05-02 10:07:09 +0600

    json data ready for display in client side

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+const chefData = require("./data/chefData.json");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Chef data are loading");
 });
 
 app.listen(port, () => {
