8b6b79a3509ee8faba5076fa797f5907fe27c719
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-05-02 11:59:30 +0600

    data added for featured food

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 const chefData = require("./data/chefData.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Chef data is loading");
