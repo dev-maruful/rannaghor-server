e6979723248a79dad206adb2b06b4412ad8245d5
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-05-02 13:37:18 +0600

    partners data added

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 
 const chefData = require("./data/chefData.json");
 
+const featuredFoods = require("./data/featuredFoods.json");
+
+const partners = require("./data/partners.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -13,6 +17,14 @@ app.get("/", (req, res) => {
 
 app.get("/chefdata", (req, res) => {
   res.send(chefData);
+});
+
+app.get("/featuredfoods", (req, res) => {
+  res.send(featuredFoods);
+});
+
+app.get("/partners", (req, res) => {
+  res.send(partners);
 });
 
 app.listen(port, () => {
